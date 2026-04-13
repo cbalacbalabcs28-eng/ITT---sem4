51a9c8cef391fc5aee83e74641732e8d5bf6e1e9
cbalacbalabcs28-eng <cbalacbala_bcs28@mepcoeng.ac.in>
2026-04-13 10:40:38 +0530

    Create lengthof rectangle.py

---
+++ simple python programs/lengthof rectangle.py
@@ -0,0 +1,7 @@
+l = int(input("Enter length:"));
+b = int(input("Enter breadth:"));
+if l>=1 and b<=100 :
+   c = l*b;
+   print("the area of the rectangle is:",c);
+else:
+   print("Invalid dimensions");
